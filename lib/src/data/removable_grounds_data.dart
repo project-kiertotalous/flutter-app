8ae8cf416a98e58de933f6acb0b30910654765b9
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-03-06 18:24:40 +0200

    add setters and remove PlutoGrid related function

--- lib/src/data/removable_grounds_data.dart
+++ lib/src/data/removable_grounds_data.dart
@@ -1,62 +1,88 @@
-import 'package:flutter_app/src/data/form_data_container.dart';
-import 'package:pluto_grid/pluto_grid.dart';
-
-class RemovableGroundsData implements FormDataContainer {
+/// User input gathered in UI forms is stored here.
+class RemovableGroundsData {
   /// Poistettavan alueen pinta-ala (m2)
   double groundSurfaceArea = 0;
+
+  void setGroundSurfaceArea(double value) {
+    groundSurfaceArea = value;
+  }
 
   /// Poistettavan alueen syvyys (m)
   double groundDepth = 0;
 
+  void setGroundDepth(double value) {
+    groundDepth = value;
+  }
+
   /// Poistettava määrä (m3)
   double removableAmount = 0;
 
+  void setRemovableAmount(double value) {
+    removableAmount = value;
+  }
+
   /// Poistettavan puhtaan maan osuus (%)
+  /// OR
+  /// Puhdas maa poistettavasta maasta (%)
   double removableCleanGroundShare = 0;
+
+  void setRemovableCleanGroundShare(double value) {
+    removableCleanGroundShare = value;
+  }
 
   /// Puhdas maa (tonnia)
   double cleanGroundTons = 0;
 
+  void setCleanGroundTons(double value) {
+    cleanGroundTons = value;
+  }
+
   /// Saastunut maa (tonnia)
   double contaminatedGroundTons = 0;
+
+  void setContaminatedGroundTons(double value) {
+    contaminatedGroundTons = value;
+  }
+
+  /// Saastunut maa poistettavasta maasta (%)
+  double contaminatedGroundShare = 0;
+
+  void setContaminatedGroundShare(double value) {
+    contaminatedGroundShare = value;
+  }
 
   /// Saastunut maa (m3)
   double contaminatedGroundCubicMeters = 0;
 
+  void setContaminatedGroundCubicMeters(double value) {
+    contaminatedGroundCubicMeters = value;
+  }
+
   /// Asfaltti alue (m2)
   double asphaltSurfaceArea = 0;
+
+  void setAsphaltSurfaceArea(double value) {
+    asphaltSurfaceArea = value;
+  }
 
   /// Asfaltti (tonnia)
   double asphaltTons = 0;
 
+  void setAsphaltTons(double value) {
+    asphaltTons = value;
+  }
+
   /// Asfaltti (m3)
   double asphaltCubicMeters = 0;
 
-  @override
-  void updateValue(PlutoGridOnChangedEvent event) {
-    final pair = [event.columnIdx, event.rowIdx];
-    final newValue = event.value;
-    switch (pair) {
-      case [1, 0]:
-        groundSurfaceArea = newValue;
-      case [1, 1]:
-        groundDepth = newValue;
-      case [1, 2]:
-        removableAmount = newValue;
-      case [1, 3]:
-        removableCleanGroundShare = newValue;
-      case [1, 4]:
-        cleanGroundTons = newValue;
-      case [1, 5]:
-        contaminatedGroundTons = newValue;
-      case [1, 6]:
-        contaminatedGroundCubicMeters = newValue;
-      case [1, 7]:
-        asphaltSurfaceArea = newValue;
-      case [1, 8]:
-        asphaltTons = newValue;
-      case [1, 9]:
-        asphaltCubicMeters = newValue;
-    }
+  void setAsphaltCubicMeters(double value) {
+    asphaltCubicMeters = value;
+  }
+
+  /// Poistettavan maan kokonaismäärä (tonnia)
+  double removableGroundTons = 0;
+
+  void setRemovableGroundTons(double value) {
+    removableGroundTons = value;
   }
 }
