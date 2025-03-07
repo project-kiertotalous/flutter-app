806553d99abfbb0f4322003100741c81e368ce4b
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-03-07 17:00:01 +0200

    add enum and setters

--- lib/src/data/foundation_type_and_foors_data.dart.dart
+++ lib/src/data/foundation_type_and_foors_data.dart.dart
@@ -1,41 +1,126 @@
 class FoundationTypeAndFloorsData {
+  /// Valesokkelin tyyppi
+  FoundationAndFloorType? fakePlinthType;
+
+  // this can still be set to null
+  void setFakePlinthType(FoundationAndFloorType? type) {
+    fakePlinthType = type;
+  }
+
+  /// Rossipohjan tyyppi
+  FoundationAndFloorType? baseFloorType;
+
+  void setBaseFloorType(FoundationAndFloorType? type) {
+    baseFloorType = type;
+  }
+
+  /// Pilariperustuksen tyyppi
+  FoundationAndFloorType? pillarFoundationType;
+
+  void setPillarFoundationType(FoundationAndFloorType? type) {
+    pillarFoundationType = type;
+  }
+
+  /// Ontelolaattoperustuksen tyyppi
+  FoundationAndFloorType? cavitySlabType;
+
+  void setCavitySlabType(FoundationAndFloorType? type) {
+    cavitySlabType = type;
+  }
+
   /// Perustuksen pinta-ala (m2) - Valesokkeli
   double? fakePlinthSurfaceArea;
+
+  void setFakePlinthSurfaceArea(double value) {
+    fakePlinthSurfaceArea = value;
+  }
 
   /// Perustuksen kehämitta (jm) - Valesokkeli
   ///
   /// jm refers to "Juoksumetri"
   double? fakePlinthCircumference;
 
+  void setfakePlinthCircumference(double value) {
+    fakePlinthCircumference = value;
+  }
+
   /// Perustuksen pinta-ala (m2) - Rossipohja
   double? baseFloorSurfaceArea;
+
+  void setBaseFloorSurfaceArea(double value) {
+    baseFloorSurfaceArea = value;
+  }
 
   /// Perustuksen kehämitta (jm) - Rossipohja
   double? baseFloorCircumference;
 
+  void setBaseFloorCircumference(double value) {
+    baseFloorCircumference = value;
+  }
+
   /// Perustuksen pinta-ala (m2) - Matalaperustus
   double? shallowFoundationSurfaceArea;
+
+  void setShallowFoundationSurfaceArea(double value) {
+    shallowFoundationSurfaceArea = value;
+  }
 
   /// Perustuksen kehämitta (jm) - Matalaperustus
   double? shallowFoundationCircumference;
 
+  void setShallowFoundationCircumference(double value) {
+    shallowFoundationCircumference = value;
+  }
+
   /// Perustuksen pinta-ala (m2) - Pilariperustus
   double? pillarFoundationSurfaceArea;
+
+  void setPillarFoundationSurfaceArea(double value) {
+    pillarFoundationSurfaceArea = value;
+  }
 
   /// Perustuksen kehämitta (jm) - Pilariperustus
   double? pillarFoundationCircumference;
 
+  void setPillarFoundationCircumference(double value) {
+    pillarFoundationCircumference = value;
+  }
+
   /// Perustuksen pinta-ala (m2) - Ontelolaattaperustus
   double? cavitySlabSurfaceArea;
+
+  void setCavitySlabSurfaceArea(double value) {
+    cavitySlabSurfaceArea = value;
+  }
 
   /// Perustuksen kehämitta (jm) - Ontelolaattaperustus
   double? cavitySlabCircumference;
 
+  void setCavitySlabCircumference(double value) {
+    cavitySlabCircumference = value;
+  }
+
   /// Perustuksen pinta-ala (m2) - Koko rakennus yhteensä
   double? buildingTotalSurfaceArea;
+
+  void setBuildingTotalSurfaceArea(double value) {
+    buildingTotalSurfaceArea = value;
+  }
 
   /// Perustuksen kehämitta (jm) - Koko rakennus yhteensä
   double? circumferenceTotal;
 
+  void setCircumferenceTotal(double value) {
+    circumferenceTotal = value;
+  }
+
   // TODO: add update function
+}
+
+/// User for menu options in [FoundationTypeAndFloorsData]
+enum FoundationAndFloorType {
+  // Betonivalu
+  concreteCasting,
+  // Harkko
+  ingot,
 }
