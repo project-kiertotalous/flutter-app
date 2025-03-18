ee4a79116daa1dd735810b743bf726264a55a23a
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-03-18 18:56:36 +0200

    add bloc classes for total building frame

---
+++ lib/src/bloc/total_building_frame_bloc.dart
@@ -0,0 +1,10 @@
+import 'package:bl_demolition_materials/bl_demolition_materials.dart';
+import 'package:flutter_app/log.dart';
+import 'package:flutter_app/src/bloc/total_building_dimensions_event.dart';
+import 'package:flutter_app/src/bloc/total_building_frame_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class TotalBuildingFrameBloc
+    extends Bloc<TotalBuildingFrameEvent, TotalBuildingFrame> {
+  TotalBuildingFrameBloc() : super(TotalBuildingFrame()) {}
+}
