3c01e8a96d09271c43730a1fd9bb720e5717e703
LSanteri <lallinsanteri0@gmail.com>
2025-03-20 23:13:01 +0200

    Added functionality for intermediate floors to take into account total building values (though right now you need to scroll down to first load in intermediate floors for it to register total building values)

--- lib/src/bloc/intermediate_floors_bloc.dart
+++ lib/src/bloc/intermediate_floors_bloc.dart
@@ -2,23 +2,35 @@ import 'package:bl_demolition_materials/bl_demolition_materials.dart';
 import 'package:flutter_app/log.dart';
 import 'package:flutter_app/src/bloc/intermediate_floors_event.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_app/src/bloc/total_building_dimensions_bloc.dart';
 
 class IntermediateFloorsBloc
     extends Bloc<IntermediateFloorsEvent, TotalIntermediateFloors> {
-  IntermediateFloorsBloc() : super(TotalIntermediateFloors()) {
-    on<WoodFramePercentageChanged>((event, emit) {
-      logger.d("WoodFramePercentageChanged ${event.value}");
+  final TotalBuildingDimensionsBloc totalBuildingDimensionsBloc;
 
-      final updatedIntermediateFloors =
-          (state.intermediateFloors ?? IntermediateFloors()).copyWith(
-        woodFramePercentageFraction: event.value,
-      );
-
-      final newState =
-          state.copyWith(intermediateFloors: updatedIntermediateFloors);
-
+  IntermediateFloorsBloc(this.totalBuildingDimensionsBloc)
+      : super(TotalIntermediateFloors()) {
+    
+    on<TotalBuildingDimensionsChanged>((event, emit) {
+      logger.d("TotalBuildingDimensionsChanged ${event.value}");
+      final newState = state.copyWith(totalBuildingDimensions: event.value);
       emit(newState);
     });
+
+    on<WoodFramePercentageChanged>((event, emit) {
+  logger.d("WoodFramePercentageChanged ${event.value}");
+
+  final updatedIntermediateFloors =
+      (state.intermediateFloors ?? IntermediateFloors()).copyWith(
+    woodFramePercentageFraction: event.value,
+  );
+
+  final newState =
+      state.copyWith(intermediateFloors: updatedIntermediateFloors);
+
+  emit(newState);
+});
+
 
     on<ConcreteCastingPercentageChanged>((event, emit) {
       logger.d("ConcreteCastingPercentageChanged ${event.value}");
@@ -62,14 +74,6 @@ class IntermediateFloorsBloc
       emit(newState);
     });
 
-    on<TotalBuildingDimensionsChanged>((event, emit) {
-      logger.d("TotalBuildingDimensionsChanged ${event.value}");
-
-      final newState = state.copyWith(totalBuildingDimensions: event.value);
-
-      emit(newState);
-    });
-
     on<TotalFoundationChanged>((event, emit) {
       logger.d("TotalFoundationChanged ${event.value}");
 
@@ -77,5 +81,13 @@ class IntermediateFloorsBloc
 
       emit(newState);
     });
+
+    // Listen to changes from TotalBuildingDimensionsBloc
+    totalBuildingDimensionsBloc.stream.listen((totalDimensionsState) {
+  if (totalDimensionsState != null) {
+    logger.d("Received update from TotalBuildingDimensionsBloc: $totalDimensionsState");
+    add(TotalBuildingDimensionsChanged(totalDimensionsState));
+  }
+});
   }
 }
