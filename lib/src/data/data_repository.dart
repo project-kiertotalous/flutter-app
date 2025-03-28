bdc714b001cd4cc35ce0506e1141bbd1ccd36530
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-03-28 20:01:43 +0200

    remove unnecessary file

--- lib/src/data/data_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:async';
-
-class DataRepository<T> {
-  final _dataStreamController = StreamController<T>();
-
-  Sink<T> get dataSink => _dataStreamController.sink;
-
-  Stream<T> get dataStream => _dataStreamController.stream;
-}
