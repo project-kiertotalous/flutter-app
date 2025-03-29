df26fea83592ef94535ac711cf1144eb6c7b1520
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-03-29 23:47:33 +0200

    style: add logos and change button styles

--- lib/src/home_view.dart
+++ lib/src/home_view.dart
@@ -7,26 +7,106 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          spacing: 40.0,
-          children: [
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/large_buildings');
-              },
-              // large buildings
-              child: Text("Suuret rakennukset"),
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/smaller_buildings');
-              },
-              // row houses and small flats
-              child: Text("Rivi- ja pienkerrostalot"),
-            ),
-          ],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: Stack(
+            children: [
+              // micropolis logo
+              Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Image.asset(
+                    'assets/images/micropolis_transparent.png',
+                    scale: 6,
+                  ),
+                ),
+              ),
+
+              // navigation buttons
+              Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  spacing: 40.0,
+                  children: [
+                    // large buildings button
+                    ElevatedButton(
+                      //   style: ElevatedButton.styleFrom(
+                      //     backgroundColor: Colors.blue,
+                      //     foregroundColor: Colors.green,
+                      //   ),
+                      style: ButtonStyle(
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                        ),
+                      ),
+                      onPressed: () =>
+                          Navigator.pushNamed(context, '/large_buildings'),
+                      child: Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Text(
+                          "Suuret rakennukset",
+                          style: TextStyle(
+                            color: Colors.black,
+                          ),
+                        ),
+                      ),
+                    ),
+                    // row houses and small flats button
+                    ElevatedButton(
+                      onPressed: () =>
+                          Navigator.pushNamed(context, '/smaller_buildings'),
+                      style: ButtonStyle(
+                        shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4.0),
+                          ),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(32.0),
+                        child: Text("Rivi- ja pienkerrostalot",
+                            style: TextStyle(
+                              color: Colors.black,
+                            )),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // sponsor logos
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    spacing: 88.0,
+                    children: [
+                      Image.asset(
+                        'assets/images/FI_co-funded by VERTICAL RGB_POS.png',
+                        scale: 6.0,
+                      ),
+                      Image.asset(
+                        'assets/images/PPL_tunnus_pysty_RGB.png',
+                        scale: 9.1,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
