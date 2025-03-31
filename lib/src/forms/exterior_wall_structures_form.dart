b7fd7b8ca116342704a7d5e98ecfe9f85410f81f
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-04-01 02:52:47 +0300

    add getters and setters

--- lib/src/forms/exterior_wall_structures_form.dart
+++ lib/src/forms/exterior_wall_structures_form.dart
@@ -1,6 +1,7 @@
 import 'package:bl_demolition_materials/bl_demolition_materials.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_app/src/bloc/total_building_frame_bloc.dart';
+import 'package:flutter_app/src/bloc/total_building_frame_event.dart';
 import 'package:flutter_app/src/data/cell.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
@@ -11,9 +12,11 @@ class OuterWallsForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final totalBuildingFrameBloc = context.read<TotalBuildingFrameBloc>();
     return BlocBuilder<TotalBuildingFrameBloc, TotalBuildingFrame>(
         builder: (context, state) {
       return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           // Exterior Wall Structures Form
           SizedBox(height: 50), // Space before next grid
@@ -34,9 +37,7 @@ class OuterWallsForm extends StatelessWidget {
             ],
             rowSizes: List.filled(14, 50.px),
             children: [
-              Cell.header(
-                initialValue: 'Seinärakenne',
-              ),
+              Cell.header(initialValue: 'Seinärakenne'),
               Cell.column(initialValue: 'Kaksinkertainen tiiliseinä'),
               Cell.column(initialValue: 'Betonielementtiseinä'),
               Cell.column(initialValue: 'Tiiliverhoiltu seinä'),
@@ -49,25 +50,101 @@ class OuterWallsForm extends StatelessWidget {
               Cell.row(
                 initialValue: 'Rakenteen osuus koko ulkoseinistä (%)',
               ),
-              Cell.output(percentage: true, getter: () => null),
-              Cell.output(percentage: true, getter: () {}),
-              Cell.input(initialValue: null, percentage: true, setter: () {}),
-              Cell.input(initialValue: null, percentage: true, setter: () {}),
-              Cell.input(initialValue: null, percentage: true, setter: () {}),
-              Cell.input(initialValue: null, percentage: true, setter: () {}),
-              Cell.input(initialValue: null, percentage: true, setter: () {}),
-              Cell.output(percentage: true, getter: () {}),
+              Cell.output(
+                percentage: true,
+                getter: () =>
+                    state.doubleLoadBearingBrickWallPart.portionPercentage,
+              ),
+              Cell.output(
+                percentage: true,
+                getter: () => state
+                    .concreteElementWallsWithoutFrameworkPart.portionPercentage,
+              ),
+              Cell.input(
+                initialValue: null,
+                percentage: true,
+                setter: (value) => totalBuildingFrameBloc.add(
+                  BuildingFrameChanged(
+                    state.buildingFrame!
+                        // TODO: unsure about this one, shouldn't it be brickVeneerWallPortionPercentage?
+                        .copyWith(brickCladWallPortionPercentage: value),
+                  ),
+                ),
+              ),
+              Cell.input(
+                initialValue: null,
+                percentage: true,
+                setter: (value) => totalBuildingFrameBloc.add(
+                  BuildingFrameChanged(
+                    state.buildingFrame!
+                        .copyWith(boardWallPortionPercentage: value),
+                  ),
+                ),
+              ),
+              Cell.input(
+                initialValue: null,
+                percentage: true,
+                setter: (value) => totalBuildingFrameBloc.add(
+                  BuildingFrameChanged(
+                    state.buildingFrame!
+                        .copyWith(profiledSheetMetalPortionPercentage: value),
+                  ),
+                ),
+              ),
+              Cell.input(
+                initialValue: null,
+                percentage: true,
+                setter: (value) => totalBuildingFrameBloc.add(
+                  BuildingFrameChanged(
+                    state.buildingFrame!.copyWith(
+                        steelProfileSandwichStructurePortionPercentage: value),
+                  ),
+                ),
+              ),
+              Cell.input(
+                initialValue: null,
+                percentage: true,
+                setter: (value) => totalBuildingFrameBloc.add(
+                  BuildingFrameChanged(
+                    state.buildingFrame!.copyWith(
+                        mineriteOrOtherStoneBoardPortionPercentage: value),
+                  ),
+                ),
+              ),
+              Cell.output(
+                percentage: true,
+                getter: () => state.totalEnvelopePartsPortionPercentage,
+              ),
               Cell.empty(),
               Cell.row(initialValue: 'Pinta-ala (m2)'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.output(
+                getter: () => state.doubleLoadBearingBrickWallPart.area,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.concreteElementWallsWithoutFrameworkPart.area,
+              ),
+              Cell.output(
+                getter: () => state.brickVeneerWallFramePart.area,
+              ),
+              Cell.output(
+                getter: () => state.woodenPlankWallFramePart.area,
+              ),
+              Cell.output(
+                getter: () => state.profiledSheetMetalFramePart.area,
+              ),
+              Cell.output(
+                getter: () => state.steelSandwichPanelFramePart.area,
+              ),
+              Cell.output(
+                getter: () => state.mineriteOrOtherStoneFramePart.area,
+              ),
+              Cell.output(
+                getter: () => state.totalEnvelopePartsArea,
+              ),
               Cell.empty(),
+
+              // second part of the form
               Cell.header(initialValue: 'Purkumateriaalien määrä'),
               Cell.empty(),
               Cell.empty(),
@@ -81,103 +158,162 @@ class OuterWallsForm extends StatelessWidget {
               Cell.row(initialValue: 'Tuulensuojalevy (bituliitti, tms.)'),
               Cell.grey(),
               Cell.grey(),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.output(
+                getter: () =>
+                    state.brickVeneerWallFramePart.windProtectionBoardTons,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.woodenPlankWallFramePart.windProtectionBoardTons,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.profiledSheetMetalFramePart.windProtectionBoardTons,
+              ),
               Cell.grey(),
               Cell.grey(),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.output(getter: () => state.windProtectionBoardVolume),
+              Cell.output(getter: () => state.windProtectionBoardTons),
               Cell.row(initialValue: 'Mineraalivilla'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.output(
+                  getter: () => state.doubleLoadBearingBrickWallPart
+                      .mineralWoolInsulationTons),
+              Cell.output(
+                getter: () => state.concreteElementWallsWithoutFrameworkPart
+                    .mineralWoolInsulationTons,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.brickVeneerWallFramePart.mineralWoolInsulationTons,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.woodenPlankWallFramePart.mineralWoolInsulationTons,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.profiledSheetMetalFramePart.mineralWoolInsulationTons,
+              ),
+              Cell.output(
+                getter: () =>
+                    state.steelSandwichPanelFramePart.mineralWoolInsulationTons,
+              ),
+              Cell.output(
+                getter: () => state
+                    .mineriteOrOtherStoneFramePart.mineralWoolInsulationTons,
+              ),
+              Cell.output(getter: () => state.mineralWoolInsulationVolume),
+              Cell.output(getter: () => state.mineralWoolInsulationTons),
               Cell.row(initialValue: '130mm kalkki- tai punatiili'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                getter: () => state.brickVeneerWallFramePart.limeOrRedBrickTons,
+              ),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(getter: () => state.limeOrRedBrickVolume),
+              Cell.output(getter: () => state.limeOrRedBrickTons),
               Cell.row(initialValue: '20m ulkoverhouslauta'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                getter: () =>
+                    state.woodenPlankWallFramePart.exteriorWoodCladdingTons,
+              ),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(getter: () => state.exteriorWoodCladdingVolume),
+              Cell.output(getter: () => state.exteriorWoodCladdingTons),
               Cell.row(initialValue: '11mm kipsilevy'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                getter: () => state.brickVeneerWallFramePart.gypsumBoardTons,
+              ),
+              Cell.output(
+                getter: () => state.woodenPlankWallFramePart.gypsumBoardTons,
+              ),
+              Cell.output(
+                getter: () => state.profiledSheetMetalFramePart.gypsumBoardTons,
+              ),
+              Cell.output(
+                getter: () => state.steelSandwichPanelFramePart.gypsumBoardTons,
+              ),
+              Cell.grey(),
+              Cell.output(getter: () => state.gypsumBoardVolume),
+              Cell.output(getter: () => state.gypsumBoardTons),
               Cell.row(initialValue: 'Profiilipelti (teräs)'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                getter: () =>
+                    state.profiledSheetMetalFramePart.profileSteelSheetTons,
+              ),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(getter: () => state.profileSteelSheetVolume),
+              Cell.output(getter: () => state.profileSteelSheetTons),
               Cell.row(initialValue: 'Puolikova kuitulevy'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                getter: () =>
+                    state.mineriteOrOtherStoneFramePart.semiHardFiberBoardTons,
+              ),
+              Cell.output(getter: () => state.semiHardFiberBoardVolume),
+              Cell.output(getter: () => state.semiHardFiberBoardTons),
               Cell.row(initialValue: 'Styrox'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.output(
+                getter: () => state
+                    .concreteElementWallsWithoutFrameworkPart.styrofoamTons,
+              ),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                  getter: () =>
+                      state.steelSandwichPanelFramePart.styrofoamTons),
+              Cell.grey(),
+              Cell.output(getter: () => state.styrofoamVolume),
+              Cell.output(getter: () => state.styrofoamTons),
               Cell.row(initialValue: 'Rappaus, sisä- ja ulkoseinät'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.output(
+                getter: () =>
+                    state.doubleLoadBearingBrickWallPart.plasterCoatingTons,
+              ),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(getter: () => state.plasterCoatingVolume),
+              Cell.output(getter: () => state.plasterCoatingTons),
               Cell.row(initialValue: 'Mineriittilevy'),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
-              Cell.output(getter: () {}),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.grey(),
+              Cell.output(
+                getter: () =>
+                    state.mineriteOrOtherStoneFramePart.mineriteBoardTons,
+              ),
+              Cell.output(getter: () => state.mineriteBoardVolume),
+              Cell.output(getter: () => state.mineriteBoardTons),
             ],
           ),
         ],
