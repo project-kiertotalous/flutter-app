e8b4795ce290b254d2222f66d0a45789f443894f
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-04-04 20:17:01 +0300

    add reusable and recyclable materials form

---
+++ lib/src/bloc/reusable_and_recyclable_materials_bloc.dart
@@ -0,0 +1,179 @@
+import 'package:bl_demolition_materials/bl_demolition_materials.dart';
+import 'package:flutter_app/src/bloc/reusable_and_recyclable_materials_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_app/log.dart';
+
+class ReusableAndRecyclableMaterialsBloc extends Bloc<
+    ReusableAndRecyclableMaterialsEvent, ReusableAndRecyclableMaterials> {
+  ReusableAndRecyclableMaterialsBloc()
+      : super(ReusableAndRecyclableMaterials()) {
+    on<CleanSoilDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          cleanSoilDemolitionCost: event.value,
+        ),
+      );
+      logger.d('cleanSoilDemolitionCost changed to ${event.value}');
+    });
+    on<AsphaltWasteDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          asphaltWasteDemolitionCost: event.value,
+        ),
+      );
+      logger.d('asphaltWasteDemolitionCost changed to ${event.value}');
+    });
+    on<CleanConcreteDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          cleanConcreteDemolitionCost: event.value,
+        ),
+      );
+      logger.d('cleanConcreteDemolitionCost changed to ${event.value}');
+    });
+    on<ConcreteBlocksDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          concreteBlocksDemolitionCost: event.value,
+        ),
+      );
+      logger.d('concreteBlocksDemolitionCost changed to ${event.value}');
+    });
+    on<WallAndRoofTilesDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          wallAndRoofTilesDemolitionCost: event.value,
+        ),
+      );
+      logger.d('wallAndRoofTilesDemolitionCost changed to ${event.value}');
+    });
+    on<CeramicTilesDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          ceramicTilesDemolitionCost: event.value,
+        ),
+      );
+      logger.d('ceramicTilesDemolitionCost changed to ${event.value}');
+    });
+    on<RebarAndSteelScrapDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          rebarAndSteelScrapDemolitionCost: event.value,
+        ),
+      );
+      logger.d('rebarAndSteelScrapDemolitionCost changed to ${event.value}');
+    });
+    on<StainlessSteelDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          stainlessSteelDemolitionCost: event.value,
+        ),
+      );
+      logger.d('stainlessSteelDemolitionCost changed to ${event.value}');
+    });
+    on<CopperDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          copperDemolitionCost: event.value,
+        ),
+      );
+      logger.d('copperDemolitionCost changed to ${event.value}');
+    });
+    on<AluminiumDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          aluminiumDemolitionCost: event.value,
+        ),
+      );
+      logger.d('aluminiumDemolitionCost changed to ${event.value}');
+    });
+    on<CleanUsableWoodDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          cleanUsableWoodDemolitionCost: event.value,
+        ),
+      );
+      logger.d('cleanUsableWoodDemolitionCost changed to ${event.value}');
+    });
+    on<CombustibleWoodWasteDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          combustibleWoodWasteDemolitionCost: event.value,
+        ),
+      );
+      logger.d('combustibleWoodWasteDemolitionCost changed to ${event.value}');
+    });
+    on<GlassDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          glassDemolitionCost: event.value,
+        ),
+      );
+      logger.d('glassDemolitionCost changed to ${event.value}');
+    });
+    on<GlassAndMineralWoolDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          glassAndMineralWoolDemolitionCost: event.value,
+        ),
+      );
+      logger.d('glassAndMineralWoolDemolitionCost changed to ${event.value}');
+    });
+    on<FiberCementBoardsDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          fiberCementBoardsDemolitionCost: event.value,
+        ),
+      );
+      logger.d('fiberCementBoardsDemolitionCost changed to ${event.value}');
+    });
+    on<GypsumBoardsDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          gypsumBoardsDemolitionCost: event.value,
+        ),
+      );
+      logger.d('gypsumBoardsDemolitionCost changed to ${event.value}');
+    });
+    on<ChipboardDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          chipboardDemolitionCost: event.value,
+        ),
+      );
+      logger.d('chipboardDemolitionCost changed to ${event.value}');
+    });
+    on<WindProtectionBoardDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          windProtectionBoardDemolitionCost: event.value,
+        ),
+      );
+      logger.d('windProtectionBoardDemolitionCost changed to ${event.value}');
+    });
+    on<EWasteDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          eWasteDemolitionCost: event.value,
+        ),
+      );
+      logger.d('eWasteDemolitionCost changed to ${event.value}');
+    });
+    on<PlasticWasteDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          plasticWasteDemolitionCost: event.value,
+        ),
+      );
+      logger.d('plasticWasteDemolitionCost changed to ${event.value}');
+    });
+    on<EnergyWasteDemolitionCostChanged>((event, emit) {
+      emit(
+        state.copyWith(
+          energyWasteDemolitionCost: event.value,
+        ),
+      );
+      logger.d('energyWasteDemolitionCost changed to ${event.value}');
+    });
+  }
+}
---
+++ lib/src/bloc/reusable_and_recyclable_materials_event.dart
@@ -0,0 +1,124 @@
+abstract class ReusableAndRecyclableMaterialsEvent {}
+
+class CleanSoilDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  CleanSoilDemolitionCostChanged(this.value);
+}
+
+class AsphaltWasteDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  AsphaltWasteDemolitionCostChanged(this.value);
+}
+
+class CleanConcreteDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  CleanConcreteDemolitionCostChanged(this.value);
+}
+
+class ConcreteBlocksDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  ConcreteBlocksDemolitionCostChanged(this.value);
+}
+
+class WallAndRoofTilesDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  WallAndRoofTilesDemolitionCostChanged(this.value);
+}
+
+class CeramicTilesDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  CeramicTilesDemolitionCostChanged(this.value);
+}
+
+class RebarAndSteelScrapDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  RebarAndSteelScrapDemolitionCostChanged(this.value);
+}
+
+class StainlessSteelDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  StainlessSteelDemolitionCostChanged(this.value);
+}
+
+class CopperDemolitionCostChanged extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  CopperDemolitionCostChanged(this.value);
+}
+
+class AluminiumDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  AluminiumDemolitionCostChanged(this.value);
+}
+
+class CleanUsableWoodDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  CleanUsableWoodDemolitionCostChanged(this.value);
+}
+
+class CombustibleWoodWasteDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  CombustibleWoodWasteDemolitionCostChanged(this.value);
+}
+
+class GlassDemolitionCostChanged extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  GlassDemolitionCostChanged(this.value);
+}
+
+class GlassAndMineralWoolDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  GlassAndMineralWoolDemolitionCostChanged(this.value);
+}
+
+class FiberCementBoardsDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  FiberCementBoardsDemolitionCostChanged(this.value);
+}
+
+class GypsumBoardsDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  GypsumBoardsDemolitionCostChanged(this.value);
+}
+
+class ChipboardDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  ChipboardDemolitionCostChanged(this.value);
+}
+
+class WindProtectionBoardDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  WindProtectionBoardDemolitionCostChanged(this.value);
+}
+
+class EWasteDemolitionCostChanged extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  EWasteDemolitionCostChanged(this.value);
+}
+
+class PlasticWasteDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  PlasticWasteDemolitionCostChanged(this.value);
+}
+
+class EnergyWasteDemolitionCostChanged
+    extends ReusableAndRecyclableMaterialsEvent {
+  num? value;
+  EnergyWasteDemolitionCostChanged(this.value);
+}
