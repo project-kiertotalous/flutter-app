60c9c9fc9dadcc4ae0a0254d36f61c5c6753df75
LSanteri <lallinsanteri0@gmail.com>
2025-04-10 12:05:15 +0300

    Created fundation bloc, event and form for small properties foundation. Full logistics

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,7 @@ import 'package:flutter_app/src/lp-bloc/total_reusable_and_recyclable_materials_
 import 'package:flutter_app/src/lp-bloc/total_roofs_bloc.dart';
 import 'package:flutter_app/src/lp-bloc/windows_bloc.dart';
 import 'package:flutter_app/src/lp-bloc/yard_and_protective_structures_bloc.dart';
+import 'package:flutter_app/src/sp-bloc/foundation_bloc.dart';
 import 'package:flutter_app/src/sp-bloc/small_property_basic_info_bloc.dart';
 import 'package:flutter_app/src/home_view.dart';
 import 'package:flutter_app/src/large_properties_view.dart';
@@ -253,7 +254,11 @@ class MyApp extends StatelessWidget {
                 BlocProvider(
                   create: (BuildContext context) =>
                       SmallPropertyBasicInfoBloc(),
-                )
+                ),
+                BlocProvider(
+                  create: (BuildContext context) =>
+                      SmallPropertiesFoundationBloc(),
+                ),
               ],
               child: SmallerPropertiesView(),
             ),
---
+++ lib/src/sp-forms/foundation_form.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/src/shared/column_cell.dart';
+import 'package:flutter_app/src/shared/input_cell.dart';
+import 'package:flutter_app/src/shared/output_cell.dart';
+import 'package:flutter_app/src/shared/row_cell.dart';
+import 'package:flutter_app/src/sp-bloc/foundation_bloc.dart';
+import 'package:flutter_app/src/sp-bloc/foundation_event.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'package:bl_demolition_materials/bl_demolition_materials.dart';
+
+class SmallPropertiesFoundationForm extends StatelessWidget {
+  const SmallPropertiesFoundationForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = context.read<SmallPropertiesFoundationBloc>();
+
+    return BlocBuilder<SmallPropertiesFoundationBloc,
+        SmallPropertiesFoundation>(builder: (context, state) {
+      return LayoutGrid(columnSizes: [
+        200.px,
+        160.px,
+        160.px,
+        160.px,
+        160.px,
+        160.px,
+        120.px,
+        120.px,
+        120.px,
+      ], rowSizes: [
+        50.px,
+        50.px,
+        50.px,
+      ], children: [
+        ColumnCell(initialValue: "Perustus"),
+        ColumnCell(initialValue: "Sokkelin pituus (jm)"),
+        ColumnCell(initialValue: "Sokkelin paksuus (otsapinnan leveys) (m)"),
+        ColumnCell(initialValue: "Betonilaatan pinta-ala (m2)"),
+        ColumnCell(initialValue: "Sokkelin korkeus (m)"),
+        ColumnCell(initialValue: "Betonilaatan paksuus (m)"),
+        ColumnCell(initialValue: "Betoni (m3)"),
+        ColumnCell(initialValue: "Betoni (tonnia)"),
+        ColumnCell(initialValue: "Betoniteräs (tonnia)"),
+        RowCell(
+            checkbox: true,
+            checkboxSetter: (value) => bloc.add(ContainsAsbestosChanged(value)),
+            checkboxTitle: "Sisältää asbestia",
+            checkboxValue: state.containsAsbestos),
+        InputCell(
+            initialValue: state.plinthLengthInLinearMeters,
+            setter: (value) => bloc.add(PlinthLengthChanged(value))),
+        InputCell(
+            initialValue: state.plinthThicknessInMeters,
+            setter: (value) => bloc.add(PlinthThicknessChanged(value))),
+        InputCell(
+            initialValue: state.concreteSlabAreaInSquareMeters,
+            setter: (value) => bloc.add(ConcreteSlabAreaChanged(value))),
+        InputCell(
+            initialValue: state.plinthHeightInMeters,
+            setter: (value) => bloc.add(PlinthHeightChanged(value))),
+        InputCell(
+            initialValue: state.concreteSlabThicknessInMeters,
+            setter: (value) => bloc.add(ConcreteSlabThicknessChanged(value))),
+        OutputCell(getter: () => state.concreteVolume),
+        OutputCell(getter: () => state.concreteTons),
+        OutputCell(getter: () => state.reinforcedConcreteTons),
+        RowCell(
+            checkbox: true,
+            checkboxSetter: (value) =>
+                bloc.add(ContainsPcbPaintsChanged(value)),
+            checkboxTitle: "Sisältää PBC maaleja",
+            checkboxValue: state.containsPcbPaints),
+      ]);
+    });
+  }
+}
