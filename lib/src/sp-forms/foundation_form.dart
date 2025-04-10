daac23d6df1353fdb97dee1b839cf03f0455abcd
LSanteri <lallinsanteri0@gmail.com>
2025-04-10 17:21:59 +0300

    Changed form a bit to be more user-friendly

--- lib/src/sp-forms/foundation_form.dart
+++ lib/src/sp-forms/foundation_form.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/shared/column_cell.dart';
+import 'package:flutter_app/src/shared/form_header.dart';
 import 'package:flutter_app/src/shared/input_cell.dart';
 import 'package:flutter_app/src/shared/output_cell.dart';
 import 'package:flutter_app/src/shared/row_cell.dart';
@@ -18,59 +19,62 @@ class SmallPropertiesFoundationForm extends StatelessWidget {
 
     return BlocBuilder<SmallPropertiesFoundationBloc,
         SmallPropertiesFoundation>(builder: (context, state) {
-      return LayoutGrid(columnSizes: [
-        200.px,
-        160.px,
-        160.px,
-        160.px,
-        160.px,
-        160.px,
-        120.px,
-        120.px,
-        120.px,
-      ], rowSizes: [
-        50.px,
-        50.px,
-        50.px,
-      ], children: [
-        ColumnCell(initialValue: "Perustus"),
-        ColumnCell(initialValue: "Sokkelin pituus (jm)"),
-        ColumnCell(initialValue: "Sokkelin paksuus (otsapinnan leveys) (m)"),
-        ColumnCell(initialValue: "Betonilaatan pinta-ala (m2)"),
-        ColumnCell(initialValue: "Sokkelin korkeus (m)"),
-        ColumnCell(initialValue: "Betonilaatan paksuus (m)"),
-        ColumnCell(initialValue: "Betoni (m3)"),
-        ColumnCell(initialValue: "Betoni (tonnia)"),
-        ColumnCell(initialValue: "Betoniteräs (tonnia)"),
-        RowCell(
-            checkbox: true,
-            checkboxSetter: (value) => bloc.add(ContainsAsbestosChanged(value)),
-            checkboxTitle: "Sisältää asbestia",
-            checkboxValue: state.containsAsbestos),
-        InputCell(
-            initialValue: state.plinthLengthInLinearMeters,
-            setter: (value) => bloc.add(PlinthLengthChanged(value))),
-        InputCell(
-            initialValue: state.plinthThicknessInMeters,
-            setter: (value) => bloc.add(PlinthThicknessChanged(value))),
-        InputCell(
-            initialValue: state.concreteSlabAreaInSquareMeters,
-            setter: (value) => bloc.add(ConcreteSlabAreaChanged(value))),
-        InputCell(
-            initialValue: state.plinthHeightInMeters,
-            setter: (value) => bloc.add(PlinthHeightChanged(value))),
-        InputCell(
-            initialValue: state.concreteSlabThicknessInMeters,
-            setter: (value) => bloc.add(ConcreteSlabThicknessChanged(value))),
-        OutputCell(getter: () => state.concreteVolume),
-        OutputCell(getter: () => state.concreteTons),
-        OutputCell(getter: () => state.reinforcedConcreteTons),
-        RowCell(
-            checkbox: true,
-            checkboxSetter: (value) =>
-                bloc.add(ContainsPcbPaintsChanged(value)),
-            checkboxTitle: "Sisältää PBC maaleja",
-            checkboxValue: state.containsPcbPaints),
+      return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        FormHeader(text: "Perustus"),
+        LayoutGrid(columnSizes: [
+          200.px,
+          160.px,
+          160.px,
+          160.px,
+          160.px,
+          160.px,
+          120.px,
+          120.px,
+          120.px,
+        ], rowSizes: [
+          50.px,
+          50.px,
+          50.px,
+        ], children: [
+          RowCell(
+              checkbox: true,
+              checkboxSetter: (value) =>
+                  bloc.add(ContainsAsbestosChanged(value)),
+              checkboxTitle: "Sisältää asbestia",
+              checkboxValue: state.containsAsbestos),
+          ColumnCell(initialValue: "Sokkelin pituus (jm)"),
+          ColumnCell(initialValue: "Sokkelin paksuus (otsapinnan leveys) (m)"),
+          ColumnCell(initialValue: "Betonilaatan pinta-ala (m2)"),
+          ColumnCell(initialValue: "Sokkelin korkeus (m)"),
+          ColumnCell(initialValue: "Betonilaatan paksuus (m)"),
+          ColumnCell(initialValue: "Betoni (m3)"),
+          ColumnCell(initialValue: "Betoni (tonnia)"),
+          ColumnCell(initialValue: "Betoniteräs (tonnia)"),
+          RowCell(
+              checkbox: true,
+              checkboxSetter: (value) =>
+                  bloc.add(ContainsPcbPaintsChanged(value)),
+              checkboxTitle: "Sisältää PBC maaleja",
+              checkboxValue: state.containsPcbPaints),
+          InputCell(
+              initialValue: state.plinthLengthInLinearMeters,
+              setter: (value) => bloc.add(PlinthLengthChanged(value))),
+          InputCell(
+              initialValue: state.plinthThicknessInMeters,
+              setter: (value) => bloc.add(PlinthThicknessChanged(value))),
+          InputCell(
+              initialValue: state.concreteSlabAreaInSquareMeters,
+              setter: (value) => bloc.add(ConcreteSlabAreaChanged(value))),
+          InputCell(
+              initialValue: state.plinthHeightInMeters,
+              setter: (value) => bloc.add(PlinthHeightChanged(value))),
+          InputCell(
+              initialValue: state.concreteSlabThicknessInMeters,
+              setter: (value) => bloc.add(ConcreteSlabThicknessChanged(value))),
+          OutputCell(getter: () => state.concreteVolume),
+          OutputCell(getter: () => state.concreteTons),
+          OutputCell(getter: () => state.reinforcedConcreteTons),
+        ])
       ]);
     });
   }
