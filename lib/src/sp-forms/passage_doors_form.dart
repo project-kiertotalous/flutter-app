c1608fe28888aefa3ed218ebf1f1b6075c9b29d5
Aleksi Kanerva <aleksi.kanerva90@gmail.com>
2025-04-15 07:38:10 +0300

    remove unused import

--- lib/src/sp-forms/passage_doors_form.dart
+++ lib/src/sp-forms/passage_doors_form.dart
@@ -1,4 +1,3 @@
-import 'package:bl_demolition_materials/bl_demolition_materials.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/src/shared/cell.dart';
 import 'package:flutter_app/src/sp-bloc/hall_doors_bloc.dart';
